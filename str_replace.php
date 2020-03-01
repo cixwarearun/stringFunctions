6f6c9b906d6f60081b741f03aeff5c218bdff2a9
Arun Bastola <cixwarearun@gmail.com>
2020-03-01 14:11:35 +0545

    str_replace is modified with count

--- str_replace.php
+++ str_replace.php
@@ -10,5 +10,8 @@
 $my_str = 'If the facts do not fit the theory, change the facts.';
  
 // replaces "facts" with "truth" and displays new string
-echo str_replace("facts", "truth", $my_str);
+echo str_replace("facts", "truth", $my_str,$count);
+echo '<br>';
+echo "the string was repalce $count times";
+
 ?>
