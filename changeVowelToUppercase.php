34a2f57421cddaa893c2bdd89bfeb16f146437f3
Arun Bastola <cixwarearun@gmail.com>
2020-03-03 13:26:47 +0545

    modified change vowel to uppercase and counting words in each sentence

--- changeVowelToUppercase.php
+++ changeVowelToUppercase.php
@@ -1,20 +1,51 @@
-<?php
-  $task =("Lorem ipsum dolor sit amet, esse consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-   consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla esse pariatur.
-   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt esse mollit anim id est laborum.");
 
-   //change vowels to uppercase
-  
-   $task1= str_replace('a','A',$task);
-   $task2= str_replace('e','E',$task1);
-   $task3= str_replace('i','I',$task2);
-   $task4= str_replace('o','O',$task3);
-   $task5= str_replace('u','U',$task4);
-   echo $task5;
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <style>
+              .a{
+                color:blue;
+              }
+              .e{
+                color:yellowgreen;
+              }
+              .i{
+                color:olivedrab;
+              }
+              .o{
+                color:palevioletred;
+              }
+              .u{
+                color:green;
+              }
 
-  
+        </style>
+      </head>
+      <body >
+                <?php
+                  $task =("Lorem ipsum dolor sit amet, esse consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla esse pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt esse mollit anim id est laborum.");
 
- 
-  
-   ?>
+                  //change vowels to uppercase
+
+                
+                  
+                $vowel= array('a','e','i','o','u');
+                $replace=array('<span class="a">A</span>','<span class="e">E</span>',
+                '<span class="i">I</span>','<span class="o">O</span>','<span class="u">U</span>');
+                echo str_replace($vowel,$replace,$task);
+          
+                    
+
+                
+                  
+                  ?>
+        
+      </body>
+      </html>
+
--- countingWordsInSentence.php
+++ countingWordsInSentence.php
@@ -2,25 +2,27 @@
   $task =("Lorem ipsum dolor sit amet, esse consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
   dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla esse pariatur.
-   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt esse mollit anim id est laborum.");
+   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt esse mollit anim id est laborum");
 
    //counting words in each sentence
    
   $task1 =  (explode('.',$task));
-  echo '<br>';
-  echo '<br>';
-  print_r('The number of words in First Sentence is'.' '.str_word_count($task1[0]));
-  echo '<br>';
-  echo '<br>';
-  print_r('The number of words in Second Sentence is'.' '.str_word_count($task1[1]));
-  echo '<br>';
-  echo '<br>';
-  print_r('The number of words in Third Sentence is'.' '.str_word_count($task1[2]));
-  echo '<br>';
-  echo '<br>';
-  print_r('The number of words in Fourth Sentence is'.' '.str_word_count($task1[3]));
+  // echo '<br>';
+  // echo '<br>';
+  // print_r('The number of words in First Sentence is'.' '.str_word_count($task1[0]));
+  // echo '<br>';
+  // echo '<br>';
+  // print_r('The number of words in Second Sentence is'.' '.str_word_count($task1[1]));
+  // echo '<br>';
+  // echo '<br>';
+  // print_r('The number of words in Third Sentence is'.' '.str_word_count($task1[2]));
+  // echo '<br>';
+  // echo '<br>';
+  // print_r('The number of words in Fourth Sentence is'.' '.str_word_count($task1[3]));
 
+  foreach($task1 as $task)
+  {
+    print_r(str_word_count($task).'<br>');
+  }
 
- 
-  
-   ?>
+ ?>
