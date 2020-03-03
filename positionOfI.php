f63792de6a9eb194c2f1653f5b9ae37f3d1d968d
Arun Bastola <cixwarearun@gmail.com>
2020-03-03 08:25:01 +0545

    Finding the position of i in given word

---
+++ positionOfI.php
@@ -0,0 +1,9 @@
+<?php
+
+//Finding the position of i
+$foo= 'exercitation';
+ echo "First position of i in {$foo} is \n".strpos($foo,'i');
+ echo '<br>';
+ echo '<br>';
+ echo "last position of i in {$foo} is \n".strripos($foo,'i');
+?>
